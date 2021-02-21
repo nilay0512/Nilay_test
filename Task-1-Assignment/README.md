61a6192a11038ee22ef04d2ad7ee8440d9077f46
nilay0512 <nilaypanchal12@gamil.com>
2021-02-20 22:52:40 -0800

    rename the Dir

---
+++ Task-1-Assignment/README.md
@@ -0,0 +1,2 @@
+# Task-1-Assignment
+Python training assignment 
---
+++ Task-1-Assignment/Task_1.py
@@ -0,0 +1,75 @@
+# Python program for variables and type of variables
+
+x= 1
+y=3.1
+z='Nilay'
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Python program to create complex variables
+
+a = 1.2
+b = -3.1
+c = complex(a,b)
+print(type(c))
+
+# Python program to swap two variable without thrird variables
+
+a=1
+b='Nilay'
+a,b = b,a
+print(a,b)
+print(type(a),type(b))
+
+# Python program to swap two variables
+
+a = 'Python'
+b = 10
+
+# To take inputs from the user
+#a = input('Enter value of a: ')
+#b = input('Enter value of b: ')
+
+# create a temporary variable c and swap the values
+c = a
+a = b
+b = c
+
+print('The value of a after swapping: {}'.format(a))
+print('The value of b after swapping: {}'.format(b))
+
+# Python Program with user input
+
+print(abs(int(input('users age :'))-int(input('2nd users age:'))))
+
+# Python program for complex
+
+a =int(input('Enter 1st Value: '))
+b =int(input('Enter 2nd Value: '))
+z = a+b
+z+=30
+print(z)
+
+# Python program to check datatype
+
+a =input('Value1 :')
+print(type(a))
+
+# Python Program to change Uper lower case
+
+
+
+# Datatype change
+
+a= int(0)
+a='Nilay'
+print(type(a))
+print(a)
+# once you reassign the value of a it will consider a latest value of a.
+
+
+
+
+
+
